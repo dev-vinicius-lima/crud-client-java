2b00c2c9c7373ee898971198969e379e33d8fd3c
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-12-04 10:42:31 -0400

    creating Repository and Entity Client

---
+++ src/main/java/com/crud/client/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.crud.client.repositories;
+
+import com.crud.client.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
